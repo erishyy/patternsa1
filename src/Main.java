e894a7f57a28d4e0039c2fe589f45a7c9b9de520
Lu <220716@astanait.edu.kz>
2023-10-02 00:03:18 +0600

    -

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,14 @@
-
 public class Main {
     public static void main(String[] args) {
-        // Используем стратегии для разных времен года
         SeasonContext summerContext = new SeasonContext(new Summer());
         SeasonContext winterContext = new SeasonContext(new Winter());
         SeasonContext autumnContext = new SeasonContext(new Autumn());
         SeasonContext springContext = new SeasonContext(new Spring());
 
-        summerContext.displaySeason(); // Вывод: Лето
-        winterContext.displaySeason(); // Вывод: Зима
-        autumnContext.displaySeason(); // Вывод: Осень
-        springContext.displaySeason(); // Вывод: Весна
+        winterContext.displaySeason();
+        springContext.displaySeason();
+        summerContext.displaySeason();
+        autumnContext.displaySeason();
     }
 }
 
